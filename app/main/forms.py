56918bb6f1204a5be04801c842b0139d57921354
TheGuyDanish <5776313+TheGuyDanish@users.noreply.github.com>
2020-09-13 08:59:44 +0000

    Fixes #2, complete certificates from CSR

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,12 @@
 """ Forms used in adventureguide """
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, PasswordField
+from wtforms import StringField, SubmitField, SelectField, PasswordField, TextAreaField,\
+                    HiddenField
 from wtforms.fields.html5 import IntegerField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
-from wtforms.validators import DataRequired, ValidationError, Email, NumberRange, Length
+from wtforms.validators import DataRequired, ValidationError, Email, NumberRange, Length,\
+                               Optional
 from app.models import User
 from app.queries import root_choice_query, intermediate_choice_query
 
@@ -75,10 +77,16 @@ class NewCertForm(FlaskForm):
     email = StringField('Email Address', validators=[DataRequired()])
     key_len = SelectField('Private Key Length',
                           choices=[('1024', '1024'), ('2048', '2048'), ('4096', '4096')],
-                          validators=[DataRequired()])
+                          validators=[Optional()])
     cert_len = IntegerField('Certificate Expiry (years)', validators=[NumberRange(min=1, max=99)])
     int_cert = QuerySelectField('Intermediate Certificate',
                                 query_factory=intermediate_choice_query, allow_blank=False,
                                 get_label='name', validators=[DataRequired()])
     int_passphrase = PasswordField('Intermediate Encryption Key', validators=[DataRequired()])
+    csr = HiddenField('CSR', validators=[Optional()])
+    submit = SubmitField('Submit')
+
+class NewCSRFulfillmentForm(FlaskForm):
+    """ Form to take a certificate request (CSR) """
+    csr = TextAreaField('CSR', validators=[DataRequired()])
     submit = SubmitField('Submit')
--- app/main/routes.py
+++ app/main/routes.py
@@ -8,7 +8,8 @@ from OpenSSL import crypto
 from app import db
 from app.certgen import createKeyPair, createCertRequest, TYPE_RSA
 from app.main import bp
-from app.main.forms import EditProfileForm, NewRootCertForm, NewIntermediateCertForm, NewCertForm
+from app.main.forms import EditProfileForm, NewRootCertForm, NewIntermediateCertForm, NewCertForm,\
+                           NewCSRFulfillmentForm
 from app.models import RootCertificate, IntermediateCertificate, Certificate
 from app.utility import has_extension
 
@@ -28,6 +29,30 @@ def asn1_to_datetime(timestamp):
 
     time_object = datetime.strptime(timestamp, current_app.config['ASN1_TIME_FORMAT'])
     return time_object.isoformat()
+
+@bp.app_template_filter('decode')
+def decode_bytes(input_bytes):
+    """ Converts bytes to UTF-8 for display purposes. """
+    string = input_bytes.decode("UTF-8")
+    return string
+
+@bp.app_template_filter('c_to_human')
+def c_to_human(string):
+    """ Convert 2-character component names to human-readable elements. """
+    if string == "C":
+        return 'Country'
+    elif string == "ST":
+        return 'State or Province'
+    elif string == "L":
+        return "Locality"
+    elif string == "O":
+        return "Organization Name"
+    elif string == "OU":
+        return "Organizational Unit"
+    elif string == "CN":
+        return "Common Name"
+    elif string == "emailAddress":
+        return "Email Address"
 
 #endregion
 
@@ -294,15 +319,22 @@ def certificate_new():
         form.organization.data = current_user.org_name
         form.ou_name.data = current_user.ou_name
     if form.validate_on_submit():
+        if form.csr.data is not None:
+            include_key = False
+        else:
+            include_key = True
         int_record = form.int_cert.data
         int_cert = crypto.load_certificate(crypto.FILETYPE_PEM, int_record.pubkey)
         fernet = Fernet(form.int_passphrase.data)
         int_privkey = fernet.decrypt(int_record.privkey.encode())
         int_key = crypto.load_privatekey(crypto.FILETYPE_PEM, int_privkey)
-        cert_key = createKeyPair(TYPE_RSA, int(form.key_len.data))
-        cert_req = createCertRequest(cert_key, CN=form.common_name.data, ST=form.state.data,
-                                     L=form.locality.data, O=form.organization.data,
-                                     OU=form.ou_name.data, emailAddress=form.email.data)
+        if include_key:
+            cert_key = createKeyPair(TYPE_RSA, int(form.key_len.data))
+            cert_req = createCertRequest(cert_key, CN=form.common_name.data, ST=form.state.data,
+                                         L=form.locality.data, O=form.organization.data,
+                                         OU=form.ou_name.data, emailAddress=form.email.data)
+        else:
+            cert_req = crypto.load_certificate_request(crypto.FILETYPE_PEM, form.csr.data.encode())
 
         cert = crypto.X509()
         cert.set_serial_number(randint(50000000, 100000000))
@@ -326,15 +358,53 @@ def certificate_new():
         cert.sign(int_key, "sha256")
 
         pubkey = crypto.dump_certificate(crypto.FILETYPE_PEM, cert).decode("UTF-8")
-        privkey = crypto.dump_privatekey(crypto.FILETYPE_PEM, cert_key).decode("UTF-8")
+        if include_key:
+            privkey = crypto.dump_privatekey(crypto.FILETYPE_PEM, cert_key).decode("UTF-8")
         final_cert = Certificate(name=form.common_name.data,
                                  pubkey=pubkey, intermediate=int_record.id)
         db.session.add(final_cert)
         db.session.commit()
-        session[f'privkey_{final_cert.id}'] = privkey
+        if include_key:
+            session[f'privkey_{final_cert.id}'] = privkey
         flash(f"Created new certificate '{form.common_name.data}'")
         return redirect(url_for('main.certificate_view', cert_id=final_cert.id))
     return render_template('certificate_new.html', form=form)
+
+@bp.route('/certificate/complete', methods=['GET', 'POST'])
+@login_required
+def certificate_new_csr():
+    """ This function accepts a standard X.509 CSR and allows the user to select an intermediate
+    to sign it with. """
+    form = NewCSRFulfillmentForm()
+    if form.validate_on_submit():
+        new_form = NewCertForm()
+        req = crypto.load_certificate_request(crypto.FILETYPE_PEM, form.csr.data.encode())
+        key = req.get_pubkey()
+        key_type = 'RSA' if key.type() == crypto.TYPE_RSA else 'DSA'
+        subject = req.get_subject()
+        components = dict(subject.get_components())
+        sanitized_components = {}
+        for info in components:
+            sanitized_components[decode_bytes(info)] = decode_bytes(components[info])
+
+        if 'CN' in sanitized_components:
+            new_form.common_name.data = sanitized_components['CN']
+        if 'C' in sanitized_components:
+            new_form.country.data = sanitized_components['C']
+        if 'ST' in sanitized_components:
+            new_form.state.data = sanitized_components['ST']
+        if 'L' in sanitized_components:
+            new_form.locality.data = sanitized_components['L']
+        if 'O' in sanitized_components:
+            new_form.organization.data = sanitized_components['O']
+        if 'OU' in sanitized_components:
+            new_form.ou_name.data = sanitized_components['OU']
+        if 'emailAddress' in sanitized_components:
+            new_form.email.data = sanitized_components['emailAddress']
+        new_form.csr.data = form.csr.data
+        return render_template('certificate_new_csr_verify.html', key=key, key_type=key_type,
+                               subject=subject, components=components, form=new_form)
+    return render_template('certificate_new_csr.html', form=form)
 
 @bp.route('/certificate/<cert_id>')
 @login_required
